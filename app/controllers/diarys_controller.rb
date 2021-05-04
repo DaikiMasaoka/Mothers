b374bff33dc598529e1e88696cb683bb33490e11
DaikiMasaoka <masadai.29@icloud.com>
2021-05-04 12:20:50 +0000

    投稿

--- app/controllers/diarys_controller.rb
+++ app/controllers/diarys_controller.rb
@@ -1,10 +1,24 @@
 class DiarysController < ApplicationController
   def index
+    @diary = Diary.new
+    @diarys = Diary.all
   end
 
   def show
+
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+    @diary.save
+    redirect_to diarys_path
   end
 
   def edit
+  end
+
+  private
+  def diary_params
+    params.require(:diary).permit(:title, :body, :image)
   end
 end
--- app/models/diary.rb
+++ app/models/diary.rb
@@ -2,4 +2,5 @@ class Diary < ApplicationRecord
   belongs_to :user
   has_many :diary_comments
   has_many :likes
+  attachment :image
 end
