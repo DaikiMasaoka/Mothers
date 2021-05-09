f05d07158ef36ff2b456f6b818848e85df7419f7
DaikiMasaoka <masadai.29@icloud.com>
2021-05-09 06:49:00 +0000

    投稿フォーム

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,20 @@
 class AnswersController < ApplicationController
+	def create
+    trouble = Trouble.find(params[:trouble_id])
+    comment = current_user.answers.new(answer_params)
+    comment.trouble_id = trouble.id
+    comment.save
+    redirect_to trouble_path(trouble)
+	end
+
+  def destroy
+    Answer.find_by(id: params[:id], trouble_id: params[:trouble_id]).destroy
+    redirect_to trouble_path(params[:trouble_id])
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:comment)
+  end
 end
--- app/controllers/troubles_controller.rb
+++ app/controllers/troubles_controller.rb
@@ -1,10 +1,39 @@
 class TroublesController < ApplicationController
   def index
+    @trouble = Trouble.new
+    @troubles = Trouble.all
   end
 
   def show
+    @trouble = Trouble.find(params[:id])
+    @answer = Answer.new
+  end
+
+  def create
+    @trouble = Trouble.new(trouble_params)
+    @trouble.user_id = current_user.id
+    @trouble.save
+    redirect_to troubles_path
   end
 
   def edit
+    @trouble = Trouble.find(params[:id])
+  end
+
+  def update
+    trouble = Trouble.find(params[:id])
+    trouble.update(trouble_params)
+    redirect_to troubles_path
+  end
+
+  def destroy
+    @trouble =Trouble.find(params[:id])
+    @trouble.destroy
+    redirect_to troubles_path
+  end
+
+  private
+  def trouble_params
+    params.require(:trouble).permit(:title, :body)
   end
 end
