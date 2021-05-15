ba911e1966bb1e5b4090e55f8a1396117d1eac9c
DaikiMasaoka <masadai.29@icloud.com>
2021-05-15 10:47:11 +0000

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   resources :troubles do
-    resources :answers
+    resources :answers, only: [:create, :destroy]
   end
   resources :diaries do
     resources :diary_comments, only: [:create, :destroy]
     resource :likes, only: [:create, :destroy]
   end
-  resources :users
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_090814) do
+ActiveRecord::Schema.define(version: 2021_05_15_064456) do
 
   create_table "answers", force: :cascade do |t|
     t.integer "trouble_id"
@@ -46,6 +46,13 @@ ActiveRecord::Schema.define(version: 2021_05_10_090814) do
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "diary_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
