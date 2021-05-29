6ad1578199ef92c22211527a28345a323784c22b
DaikiMasaoka <masadai.29@icloud.com>
2021-05-29 11:07:38 +0000

    about

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-  root to: 'homes#top'
   resources :troubles do
     resources :answers, only: [:create, :destroy]
   end
